e6418136829db463824e3a71a59ee12ea9d68e5e
suguru-3u <suguru.13579515@gmail.com>
2025-11-13 21:08:22 +0900

    Spring Securityの導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
