b9fb55881cf24a3ff38e91964ea678461c22a3fe
suguru-3u <suguru.13579515@gmail.com>
2025-12-09 08:25:36 +0900

    TODO一覧取得APIのIntegrationテストの実装

--- src/integrationTest/kotlin/com/example/todo_app_backend/controller/TodoIndexTest.kt
+++ src/integrationTest/kotlin/com/example/todo_app_backend/controller/TodoIndexTest.kt
@@ -11,31 +11,42 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class Sample : BaseIntegrationTest() {
+class TodoIndexTest : BaseIntegrationTest() {
 
     @Autowired
     lateinit var restTemplate: TestRestTemplate
 
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    @BeforeTest
+    fun setUp() {
+        val sql = "INSERT INTO todo (text, accountId) VALUES ('Todo Index IntegrationTest', 'user')"
+        jdbcTemplate.execute(sql)
+    }
+
+    @AfterTest
+    fun cleanup() {
+        val sql = "DELETE FROM todo WHERE text = 'Todo Index IntegrationTest'"
+        jdbcTemplate.execute(sql)
+    }
+
     @Test
     fun contextLoads() {
-        println("Sample test running")
-
-        val base = Base(restTemplate)  // ← 自分でインスタンス化
-
+        // ログインAPIの実行
+        val base = Base(restTemplate)
         val result = base.contextLoads()
 
-        println("status = ${result?.statusCode}")
-        println("headers = ${result?.headers}")
-        println("body = ${result?.body}")
-
-
-        // when
+        // セッションの取得
         val headers = HttpHeaders()
         headers.set("Cookie", result!!.headers["Set-Cookie"]!!.joinToString())
         val entity = HttpEntity(null, headers)
 
+        // リクエストの生成&実行
         val response = restTemplate.exchange(
             "/api/todos",
             HttpMethod.GET,
@@ -43,10 +54,10 @@ class Sample : BaseIntegrationTest() {
             Array<TodoController.TodoItem>::class.java
         )
 
-        // then
+        // 検証
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
         val todos = response.body!!
-        println("todos = ${todos.size}")
-        assertThat(todos).isEmpty()
+        assertThat(todos).hasSize(1)
+        assertThat(todos[0].text).isEqualTo("Todo Index IntegrationTest")
     }
 }
