9d75aa60e0c6bce51ef59019efbb58a2f720f1a0
suguru-3u <suguru.13579515@gmail.com>
2026-01-01 09:55:28 +0900

    ユーザーを新規登録できるように改修

--- src/main/kotlin/com/example/todo_app_backend/usecase/user/UserRegisterUseCase.kt
+++ src/main/kotlin/com/example/todo_app_backend/usecase/user/UserRegisterUseCase.kt
@@ -1,14 +1,16 @@
 package com.example.todo_app_backend.usecase.user
 
-import com.example.todo_app_backend.domain.User.UserFindService
 import com.example.todo_app_backend.domain.User.RegisterUser
+import com.example.todo_app_backend.domain.User.UserFindService
+import com.example.todo_app_backend.repository.user.UserRepository
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserRegisterUseCase(
     private val passwordEncoder: PasswordEncoder,
-    private val userFindService: UserFindService
+    private val userFindService: UserFindService,
+    private val userRepository: UserRepository
 ) {
 
     fun execute(username: RegisterUser.Username, password: RegisterUser.Password) {
@@ -23,11 +25,11 @@ class UserRegisterUseCase(
             passwordEncoder = passwordEncoder
         )
 
-        val registerUser = RegisterUser(
-            username = username,
-            password = hashedPassword
+        userRepository.registerUser(
+            registerUser = RegisterUser(
+                username = username,
+                password = hashedPassword
+            )
         )
-
-        // ユーザーを登録する
     }
 }
